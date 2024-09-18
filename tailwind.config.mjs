85d7e2ff1f5a67785226ce7aeda1b3890c976252
sunnywx <iwisunny@gmail.com>
2024-09-18 23:53:16 +0800

    feat: add postman react components

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+// import type { Config } from "tailwindcss";
+
+const config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+};
+export default config;
